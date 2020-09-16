9e543882ccf4c4149d4f73d3890f611716651912
yasin6253952 <yasil.fatullayev@gmail.com>
2020-09-16 13:49:07 -0400

    Update test

--- integration-testing/tests/find-users.test.js
+++ integration-testing/tests/find-users.test.js
@@ -10,6 +10,7 @@ beforeAll(async () => {
   loginCache.addCleanLogin(await cognito.getAppSyncLogin())
   loginCache.addCleanLogin(await cognito.getAppSyncLogin())
   loginCache.addCleanLogin(await cognito.getAppSyncLogin())
+  loginCache.addCleanLogin(await cognito.getAppSyncLogin())
 })
 
 beforeEach(async () => await loginCache.clean())
@@ -17,26 +18,24 @@ afterAll(async () => await loginCache.reset())
 
 test('Find users by email & phoneNumber too many', async () => {
   const {client, email} = await loginCache.getCleanLogin()
-  let emails = []
-  for (let i = 0; i < 101; i++) emails[i] = `${uuidv4()}${email}`
+  const emails = Array(101).fill(email)
 
   await misc.sleep(2000)
   await expect(client.query({query: queries.findUsers, variables: {emails}})).rejects.toThrow(
-    'GraphQL error: ServerError: Max 100 items per batch get request',
+    /Cannot submit more than 100 combined emails and phoneNumbers/,
   )
 })
 
 test('Find users can handle duplicate emails', async () => {
-  const {client} = await loginCache.getCleanLogin()
-  const {userId: otherUserId, email: otherEmail, username: otherUsername} = await cognito.getAppSyncLogin()
+  const {client, userId, email, username} = await loginCache.getCleanLogin()
 
   await misc.sleep(2000)
   await client
-    .query({query: queries.findUsers, variables: {emails: [otherEmail, otherEmail]}})
+    .query({query: queries.findUsers, variables: {emails: [email, email]}})
     .then(({data: {findUsers}}) => {
       expect(findUsers.items).toHaveLength(1)
-      expect(findUsers.items[0].userId).toBe(otherUserId)
-      expect(findUsers.items[0].username).toBe(otherUsername)
+      expect(findUsers.items[0].userId).toBe(userId)
+      expect(findUsers.items[0].username).toBe(username)
     })
 })
 
@@ -46,11 +45,11 @@ test('Find users by email', async () => {
     userId: ourUserId,
     email: ourEmail,
     username: ourUsername,
-  } = await cognito.getAppSyncLogin()
+  } = await loginCache.getCleanLogin()
 
-  const {userId: other1UserId, email: other1Email, username: other1Username} = await cognito.getAppSyncLogin()
-  const {userId: other2UserId, email: other2Email, username: other2Username} = await cognito.getAppSyncLogin()
-  const cmp = (a, b) => (a.userId < b.userId ? 1 : -1)
+  const {userId: other1UserId, email: other1Email, username: other1Username} = await loginCache.getCleanLogin()
+  const {userId: other2UserId, email: other2Email, username: other2Username} = await loginCache.getCleanLogin()
+  const cmp = (a, b) => a.userId < b.userId ? 1 : -1
 
   // how each user will appear in search results, based on our query
   const us = {__typename: 'User', userId: ourUserId, username: ourUsername}
@@ -67,17 +66,14 @@ test('Find users by email', async () => {
     .then(({data: {findUsers}}) => expect(findUsers.items).toEqual([]))
 
   // find one user
-  await misc.sleep(2000)
   await ourClient
     .query({query: queries.findUsers, variables: {emails: [other1Email]}})
     .then(({data: {findUsers}}) => expect(findUsers.items).toEqual([other1]))
-  await misc.sleep(2000)
   await ourClient
     .query({query: queries.findUsers, variables: {emails: [ourEmail, 'AA' + other1Email]}})
     .then(({data: {findUsers}}) => expect(findUsers.items).toEqual([us]))
 
   // find multiple users
-  await misc.sleep(2000)
   await ourClient
     .query({query: queries.findUsers, variables: {emails: [ourEmail, other1Email, other2Email]}})
     .then(({data: {findUsers}}) => expect(findUsers.items.sort(cmp)).toEqual([us, other1, other2].sort(cmp)))
@@ -89,24 +85,24 @@ test('Find users by phone, and by phone and email', async () => {
     userId: ourUserId,
     email: ourEmail,
     username: ourUsername,
-  } = await cognito.getAppSyncLogin()
+  } = await loginCache.getCleanLogin()
   const theirPhone = '+15105551011'
   const {userId: theirUserId, email: theirEmail, username: theirUsername} = await cognito.getAppSyncLogin(
     theirPhone,
   )
-  const cmp = (a, b) => (a.userId < b.userId ? 1 : -1)
+  const cmp = (a, b) => a.userId < b.userId ? 1 : -1
 
   // how each user will appear in search results, based on our query
   const us = {__typename: 'User', userId: ourUserId, username: ourUsername}
   const them = {__typename: 'User', userId: theirUserId, username: theirUsername}
 
   // find them by just phone
+  await misc.sleep(2000)
   await ourClient
     .query({query: queries.findUsers, variables: {phoneNumbers: [theirPhone]}})
     .then(({data: {findUsers}}) => expect(findUsers.items).toEqual([them]))
 
   // find us and them by phone and email, make sure they don't duplicate
-  await misc.sleep(2000)
   await ourClient
     .query({query: queries.findUsers, variables: {emails: [ourEmail, theirEmail], phoneNumbers: [theirPhone]}})
     .then(({data: {findUsers}}) => expect(findUsers.items.sort(cmp)).toEqual([us, them].sort(cmp)))
@@ -118,18 +114,18 @@ test('Find Users sends cards to the users that were found', async () => {
     userId: ourUserId,
     email: ourEmail,
     username: ourUsername,
-  } = await cognito.getAppSyncLogin()
+  } = await loginCache.getCleanLogin()
 
-  const {client: otherClient, userId: otherUserId, email: otherEmail} = await cognito.getAppSyncLogin()
+  const {client: otherClient, userId: otherUserId, email: otherEmail} = await loginCache.getCleanLogin()
 
   const {
     client: other1Client,
     userId: other1UserId,
     email: other1Email,
     username: other1Username,
-  } = await cognito.getAppSyncLogin()
+  } = await loginCache.getCleanLogin()
 
-  const {client: other2Client, userId: other2UserId, email: other2Email} = await cognito.getAppSyncLogin()
+  const {client: other2Client, userId: other2UserId, email: other2Email} = await loginCache.getCleanLogin()
 
   const randomEmail = `${uuidv4()}@real.app`
 
@@ -143,16 +139,9 @@ test('Find Users sends cards to the users that were found', async () => {
       expect(findUsers.items).toEqual([other1])
     })
 
-  // check not-called user has no card
-  await other2Client.query({query: queries.self}).then(({data: {self}}) => {
-    expect(self.userId).toBe(other2UserId)
-    expect(self.cardCount).toBe(0)
-  })
-
   // check called user has card
   const cardId = await other1Client.query({query: queries.self}).then(({data: {self}}) => {
     expect(self.userId).toBe(other1UserId)
-    expect(self.cardCount).toBe(1)
     const card = self.cards.items[0]
     expect(card.cardId).toBe(`${other1UserId}:NEW_FOLLOWER:${ourUserId}`)
     expect(card.title).toBe(`${ourUsername} joined REAL`)
@@ -167,7 +156,6 @@ test('Find Users sends cards to the users that were found', async () => {
     .then(({data}) => expect(data.deleteCard.cardId).toBe(cardId))
 
   // find different Users with new user
-  await misc.sleep(2000)
   await ourClient
     .query({query: queries.findUsers, variables: {emails: [ourEmail, other1Email, other2Email]}})
     .then(({data: {findUsers}}) => {
@@ -176,6 +164,7 @@ test('Find Users sends cards to the users that were found', async () => {
       )
     })
   // check first called user has card
+  await misc.sleep(2000)
   await other1Client.query({query: queries.self}).then(({data: {self}}) => {
     expect(self.userId).toBe(other1UserId)
     expect(self.cards.items[0].cardId).toBe(`${other1UserId}:NEW_FOLLOWER:${ourUserId}`)
